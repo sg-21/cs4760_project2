09ee2ff51249cc7be0701e18ad2ceedf70680026
sg-21 <78775546+sg-21@users.noreply.github.com>
2021-03-22 23:30:22 -0500

    Update same.h

--- same.h
+++ same.h
@@ -13,60 +13,59 @@
 
 extern int opterr;
 
-//semiphore union
+/*semiphore union*/
 union semun {
-    int val;
-    struct semid_ds *buf;
-    unsigned short *array;
+    int                 val;
+    struct semid_ds *   buf;
+    unsigned short *    array;
 
 #if defined(__linux__)
-    struct seminfo *__buf;
+    struct seminfo *    __buf;
 #endif
 
 };
 
-//shared memory 
+const int maxChild = 20;
+const int maxSecond = 100;
+
+/*shared memory*/
 struct SharedItem {
-    bool ready;       //ready to process           
-    pid_t pid;        //in process     
-    bool finished;    //complete process         
-    int nodeDepth;    //depth node processing         
-    int value;        //actual value             
-    state nodeState;  //critical section flag       
+    bool choosing[maxChild];
+    int number[maxChild];
+    bool ready;            
+    pid_t pid;              
+    bool finished;          
+    int nodeDepth;          
+    int value;                      
 };
 
-key_t key = 0;        //shared key
+/*shared key*/
+key_t key = 0;  
 int shm_id; 
 char* shm_addr;
 
-const char* host = "./master";
-const char* child = "./bin_adder";
+const char* Host = "./master";
+const char* Child = "./bin_adder";
 
-//time formatting
+/*return string from int*/
 string GetStringFromInt(const int nVal) {
     int length = snprintf( NULL, 0, "%d", nVal);
-    char* sdep = (char*)malloc( length + 1 );
-
-    snprintf( sdep, length + 1, "%d", nVal);
-    string strFinalVal = sdep;
-
-    free(sdep);
+    char* sDep = (char*)malloc( length + 1 );
+    snprintf( sDep, length + 1, "%d", nVal);
+    string strFinalVal = sDep;
+    free(sDep);
     return strFinalVal;
 }
 
-//return string from int
-string GetTimeFormatted(const char* prePendString) {
-    time_t raw_t;
-    struct tm * timeinfo;
-    char buffer[10];
+/*time formatting*/
+char* getTimeFormatted() {
 
-    time (&raw_t);
-    timeinfo = localtime (&raw_t);
+	int timeLength = 50;
+        char* formatTime = new char[timeLength];
+        time_t now = time(NULL);
 
-    strftime (buffer, 80, "%T", timeinfo);
+        strftime(formatTime, timeLength, "%H:%M:%S", localtime(&now));
+        return formatTime;
 
-    string strReturn = prePendString;
-    strReturn.append(buffer);
-    return strReturn;
 }
 
